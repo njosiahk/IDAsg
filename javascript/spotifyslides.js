c9c1d27c5308f787912baeb29285f4a3b252d011
njosiahk <s10244345@connect.np.edu.sg>
2022-11-29 12:01:54 +0800

    discography page done unless more albums to add

---
+++ javascript/spotifyslides.js
@@ -0,0 +1,30 @@
+var slideIndex = 0;
+showSlides();
+var slides;
+
+function showSlides() {
+  let i;
+  slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 30000); // Change image every 30 seconds
+}
+
+function plusSlides(position) {
+  slideIndex += position;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  } else if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
